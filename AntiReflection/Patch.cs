0759ae155ce3cbb93aefe4c7367c42dee656744d
DragonFire47 <1redyoung@gmail.com>
2021-03-21 13:04:52 -0400

    Add project files.

---
+++ AntiReflection/Patch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace AntiReflection
+{
+    [HarmonyPatch(typeof(PLServer), "OnPhotonSerializeView")]
+    class Patch
+    {
+        static void Postfix(PLServer __instance)
+        {
+            __instance.IsReflection = false;
+        }
+    }
+}
---
+++ AntiReflection/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace AntiReflection
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "0.0.1";
+
+        public override string Author => "Dragon";
+
+        public override string LongDescription => "Text";
+
+        public override string Name => "AntiReflection";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.AntiReflection";
+        }
+    }
+}
