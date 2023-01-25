0db2eafb0faa4f8c100f9e242b60ef4311eb411e
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2023-01-25 15:41:24 -0800

    Added Command

---
+++ AntiReflection/Commands.cs
@@ -0,0 +1,31 @@
+﻿using PulsarModLoader.Chat.Commands.CommandRouter;
+using PulsarModLoader.Utilities;
+
+namespace AntiReflection
+{
+    internal class Commands : ChatCommand
+    {
+        public override string[] CommandAliases()
+        {
+            return new string[] { "antireflection", "af" };
+        }
+
+        public override string Description()
+        {
+            return "Toggles reflection and reflection transition setting. Use /af reflection and /af transition";
+        }
+
+        public override void Execute(string arguments)
+        {
+            if(arguments.ToLower().StartsWith("t"))
+            {
+                Global.CanRenderReflectionTransition.Value = !Global.CanRenderReflectionTransition.Value;
+            }
+            else if (arguments.ToLower().StartsWith("r"))
+            {
+                Global.CanRenderReflection.Value = !Global.CanRenderReflection.Value;
+            }
+            Messaging.Notification($"Reflection: {(Global.CanRenderReflection.Value ? "Enabled" : "Disabled")}\nTransition: {(Global.CanRenderReflectionTransition.Value ? "Enabled" : "Disabled")}");
+        }
+    }
+}
